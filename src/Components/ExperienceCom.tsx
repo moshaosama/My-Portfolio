b85c9387df9adda19140ff803c7640a82c2c40d5
moshaosama <mohamedOSFekry@gmail.com>
2024-12-16 17:01:12 +0200

    Edit

---
+++ src/Components/ExperienceCom.tsx
@@ -0,0 +1,53 @@
+interface Experience {
+  Label: string;
+  Title: string;
+  Description: string;
+  Src: string;
+  date: string;
+  Padding: string;
+}
+
+const ExperienceCom = ({
+  Title,
+  Label,
+  Src,
+  date,
+  Description,
+  Padding,
+}: Experience) => {
+  const Color = localStorage.getItem("Color");
+  return (
+    <>
+      <div
+        className={`my-6 max-h-[40pc] border-white border-solid border rounded-xl w-[35pc] max-sm:w-72  transition-all duration-300  ${
+          Color == "white"
+            ? "hover:shadow-[#ddd] hover:shadow-xl border-black"
+            : "border-white "
+        }`}
+      >
+        <div className="bg-blue-500 flex justify-center p-14 rounded-xl">
+          <h1 className="text-xl font-bold">{Label}</h1>
+        </div>
+        <div className="-my-16 flex justify-center">
+          <img src={Src} alt="" className={`w-32 py-${Padding}`} />
+        </div>
+        <div className="flex flex-col items-center gap-2  justify-center my-14">
+          <h1 className="text-3xl font-bold max-sm:text-xl">{Title}</h1>
+          <p className="text-2xl max-sm:text-xl">{date}</p>
+          <ul
+            className={`${
+              Color == "white" ? "text-black" : "text-white"
+            } flex flex-col  justify-center my-10`}
+          >
+            <li>{Description}</li>
+            <a href="#" className="text-blue-500">
+              Ream More...
+            </a>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ExperienceCom;
--- src/Components/Sections/Experience.tsx
+++ src/Components/Sections/Experience.tsx
@@ -1,3 +1,5 @@
+import ExperienceCom from "../ExperienceCom";
+
 const Experience = () => {
   const Color = localStorage.getItem("Color");
   return (
@@ -10,111 +12,30 @@ const Experience = () => {
       >
         <h1 className=" text-5xl max-sm:text-3xl">Experiences</h1>
         <div className="flex max-sm:flex-col max-sm:justify-center items-center gap-4">
-          <div
-            className={`my-6 max-h-96 border-white border-solid border rounded-xl w-[35pc] max-sm:w-72  transition-all duration-300  ${
-              Color == "white"
-                ? "hover:shadow-black hover:shadow-lg border-black"
-                : "border-white"
-            }`}
-          >
-            <div className="bg-blue-500 flex justify-center p-14 rounded-xl">
-              <h1 className="text-xl font-bold">Azzrk</h1>
-            </div>
-            <div className="-my-16 flex justify-center">
-              <img
-                src="azzrkplatform_logo-removebg-preview.png"
-                alt=""
-                className="w-32"
-              />
-            </div>
-            <div className="flex flex-col items-center gap-2 justify-center my-14">
-              <h1 className="text-3xl font-bold max-sm:text-xl">
-                Front-End Developer
-              </h1>
-              <p className="text-2xl max-sm:text-xl">Dec 2024 – Present</p>
-              <ul
-                className={`${
-                  Color == "white" ? "text-black" : "text-white"
-                } flex flex-col  justify-center my-10`}
-              >
-                <li>
-                  Delivered educational sessions HTML, CSS, JavaScript within
-                  the GDSC.
-                </li>
-                <a href="#" className="text-blue-500">
-                  Ream More...
-                </a>
-              </ul>
-            </div>
-          </div>
-          <div
-            className={`my-6 max-h-96 border-white border-solid border rounded-xl w-[35pc] max-sm:w-72  transition-all duration-300  ${
-              Color == "white"
-                ? "hover:shadow-black hover:shadow-lg border-black"
-                : "border-white"
-            }`}
-          >
-            <div className="bg-[#ddd] flex justify-center p-14 rounded-xl">
-              <h1 className="text-xl font-bold text-black">GDSC</h1>
-            </div>
-            <div className="-my-16 flex justify-center">
-              <img
-                src="gdsc_akhbar_el_youm_logo-removebg-preview.png"
-                alt=""
-                className="w-32"
-              />
-            </div>
-            <div className="flex flex-col items-center gap-2 justify-center my-14">
-              <h1 className="text-3xl font-bold max-sm:text-xl">
-                Front-End Developer (Instructor)
-              </h1>
-              <p className="text-2xl max-sm:text-xl">Sep 2023 – June 2024</p>
-              <ul
-                className={`${
-                  Color == "white" ? "text-black" : "text-white"
-                } flex flex-col  justify-center my-10`}
-              >
-                <li>
-                  Delivered educational sessions HTML, CSS, JavaScript within
-                  the GDSC.
-                </li>
-                <a href="#" className="text-blue-500">
-                  Ream More...
-                </a>
-              </ul>
-            </div>
-          </div>
-          <div
-            className={`my-6 max-h-96 border-white border-solid border rounded-xl w-[35pc] max-sm:w-72  transition-all duration-300  ${
-              Color == "white"
-                ? "hover:shadow-black hover:shadow-lg border-black"
-                : "border-white"
-            }`}
-          >
-            <div className="bg-green-300 flex justify-center p-14 rounded-xl">
-              <h1 className="text-xl font-bold text-black">Fetra App</h1>
-            </div>
-
-            <div className="flex flex-col items-center gap-2 justify-center my-14">
-              <h1 className="text-3xl font-bold max-sm:text-xl">
-                Front-End Developer (FreeLancer)
-              </h1>
-              <p className="text-2xl max-sm:text-xl">May 2023 – Nov 2023</p>
-              <ul
-                className={`${
-                  Color == "white" ? "text-black" : "text-white"
-                } flex flex-col  justify-center my-10`}
-              >
-                <li>
-                  Delivered educational sessions HTML, CSS, JavaScript within
-                  the GDSC.
-                </li>
-                <a href="#" className="text-blue-500">
-                  Ream More...
-                </a>
-              </ul>
-            </div>
-          </div>
+          <ExperienceCom
+            Label="Azzrk"
+            Src="azzrkplatform_logo-removebg-preview.png"
+            Title="Front End Developer"
+            date="Dec 2024 – Present"
+            Description="Contributed to educational sessions on HTML, CSS, and JavaScript..."
+            Padding=""
+          />
+          <ExperienceCom
+            Label="GDSC"
+            Src="gdsc_akhbar_el_youm_logo-removebg-preview.png"
+            Title="Front End Developer (Instructor)"
+            date="Sep 2023 – June 2024"
+            Description="Delivered educational sessions HTML, CSS, JavaScript within the GDSC..."
+            Padding=""
+          />
+          <ExperienceCom
+            Label="Fetra App"
+            Src="WhatsApp_Image_2024-04-13_at_21.23.38_37e06ac1-removebg-preview.png"
+            Title="Front End Developer (FreeLancer)"
+            date="May 2023 – Nov 2023"
+            Description="Gained experience in effective communication through this project...."
+            Padding="10"
+          />
         </div>
       </div>
     </>
